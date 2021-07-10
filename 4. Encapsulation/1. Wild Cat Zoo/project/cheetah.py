10fc452742005349334ec9ab8c0c7f06349e8595
Arksiana <74001470+Arksiana@users.noreply.github.com>
2021-07-10 14:30:27 +0300

    fix

--- 4. Encapsulation/1. Wild Cat Zoo/project/cheetah.py
+++ 4. Encapsulation/1. Wild Cat Zoo/project/cheetah.py
@@ -2,11 +2,7 @@ from project.animal import Animal
 
 
 class Cheetah(Animal):
-    needs = 60
 
     def __init__(self, name, gender, age):
-        super().__init__(name, gender, age)
+        super().__init__(name, gender, age, 60)
 
-    @staticmethod
-    def get_needs():
-        return Cheetah.needs
